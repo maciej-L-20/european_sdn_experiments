1ead90a6140a3896f2dd359b6aa373469dedcffd
Maciej Lipski <maciej.lipski2.stud@pw.edu.pl>
2023-11-10 17:22:19 +0100

    dijkstra for bw and delay

--- scriptFIles/flowApp.py
+++ scriptFIles/flowApp.py
@@ -1,11 +1,40 @@
-#TODO: 1. Reprezentacja grafu
 #TODO: 2. Coś co wczytuje rządania
 #TODO: 3. Przetwarzanie rządań
+#TODO: 3.1 podaje je w formie do wysłania onosowi
+#TODO: 3.2 aktualizacja grafu
+
 
 import heapq
 import json
 
-def dijkstra(graph, start):
+def bw_dijkstra(graph, start):
+    # Inicjalizacja odległości od startowego wierzchołka do pozostałych
+    distances = {vertex: [0,[]] for vertex in graph}
+    distances[start][0] = 0
+
+    # Kolejka priorytetowa do przechowywania wierzchołków i ich odległości
+    priority_queue = [(float('infinity'), start)]
+
+    while priority_queue:
+        current_distance, current_vertex = heapq.heappop(priority_queue)
+        current_path = distances[current_vertex][1][:]
+        current_path.append(current_vertex)
+        # Pominięcie wierzchołków, które zostały już odwiedzone
+        if current_distance < distances[current_vertex][0]:
+            continue
+
+        # Iteracja po sąsiadach aktualnego wierzchołka
+        for neighbor, weight, _ in graph[current_vertex]:
+            distance = min(current_distance,weight)
+
+            # Aktualizacja odległości, jeśli znaleziono krótszą ścieżkę
+            if distance > distances[neighbor][0]:
+                distances[neighbor][0] = distance
+                distances[neighbor][1] = current_path
+                heapq.heappush(priority_queue, (distance, neighbor))
+    return distances
+
+def delay_dijkstra(graph, start):
     # Inicjalizacja odległości od startowego wierzchołka do pozostałych
     distances = {vertex: [float('infinity'),[]] for vertex in graph}
     distances[start][0] = 0
@@ -22,7 +51,7 @@ def dijkstra(graph, start):
             continue
 
         # Iteracja po sąsiadach aktualnego wierzchołka
-        for neighbor, weight in graph[current_vertex]:
+        for neighbor, _ , weight in graph[current_vertex]:
             distance = current_distance + weight
 
             # Aktualizacja odległości, jeśli znaleziono krótszą ścieżkę
@@ -32,22 +61,18 @@ def dijkstra(graph, start):
                 heapq.heappush(priority_queue, (distance, neighbor))
     return distances
 
-def delay_dijkstra(graph,start):
-    for node_links  in graph.values():
-        for link in node_links:
-            link.pop(1)
-    return dijkstra(graph,start)
-
 
 graph_file = open('graphLinks.json','r')
+
 
 # Przykładowy graf
 graph = json.load(graph_file)
 # Wierzchołek początkowy
-start_vertex = "s1"
+start_vertex = "s3"
 
 
 shortest_paths = delay_dijkstra(graph, start_vertex)
+shortest_paths = bw_dijkstra(graph, start_vertex)
 
 for vertex, distance in shortest_paths.items():
     print(f"Najkrótsza ścieżka od {start_vertex} do {vertex} wynosi {distance}")
